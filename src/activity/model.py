ba08a1cbfa89b52e3a26d87c453431898a8d3c53
Adithya <aditsom@gmail.com>
2026-04-29 13:53:15 -0700

    activity description

--- src/activity/model.py
+++ src/activity/model.py
@@ -1,16 +1,16 @@
 from app import db
-import enum
 from datetime import datetime
 
-from sqlalchemy import Enum, ForeignKey, Index, text
+from sqlalchemy import ForeignKey
 from sqlalchemy.sql.schema import Column
-from sqlalchemy.sql.sqltypes import Boolean, DateTime, Integer, String, Date
+from sqlalchemy.sql.sqltypes import DateTime, Integer, Date, String
 
 
 class Activity(db.Model):
     id = Column(Integer, primary_key=True)
     date = Column(Date, nullable=False)
     units = Column(Integer, nullable=True)
+    description = Column(String(255), nullable=True)
     habit_id = Column(ForeignKey("habit.id"), nullable=False)
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, default=datetime.now())
