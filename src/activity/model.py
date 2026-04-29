a4de6e35ab8970ec39e6f7a8b798ea41483cfb04
adithya-somasundaram <43027530+adithya-somasundaram@users.noreply.github.com>
2026-04-29 13:56:21 -0700

    Habit tracker/activity model (#4)
    * model
    
    * activity description
    
    * remove updated_at

---
+++ src/activity/model.py
@@ -0,0 +1,15 @@
+from app import db
+from datetime import datetime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import DateTime, Integer, Date, String
+
+
+class Activity(db.Model):
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    units = Column(Integer, nullable=True)
+    description = Column(String(255), nullable=True)
+    habit_id = Column(ForeignKey("habit.id"), nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
