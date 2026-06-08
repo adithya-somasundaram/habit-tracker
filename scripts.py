846729c60bb2c9e8f4a040519cbd588118b56349
Adithya <aditsom@gmail.com>
2026-06-07 17:17:20 -0700

    nice

--- scripts.py
+++ scripts.py
@@ -1,11 +1,6 @@
 from app import *
-from src.habit.services import (
-    create_habit_input,
-    create_habits_bulk_input,
-    view_habits,
-)
-from src.habit.infra import deactivate_habit
-from src.activity.services import create_activity_input
+from src.habit.services import *
+from src.activity.services import *
 
 app.app_context().push()
 db.create_all()
